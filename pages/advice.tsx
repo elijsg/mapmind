b42f7289ba5fa4557fc1f659fd1978a738de8ee7
elijsg <elijahsgregory@gmail.com>
2023-05-28 15:38:23 -0700

    GPT Read 3

--- pages/advice.tsx
+++ pages/advice.tsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState, useContext } from 'react';
 import { GlobalStateContext } from '../context/GlobalStateContext';
 import { extractRelevantTopics } from './extract';
 import { searchTopics } from './search';
+import axios from 'axios';
 
 const AdvicePage = () => {
   const globalState = useContext(GlobalStateContext);
   const [advice, setAdvice] = useState("");
   const [topics, setTopics] = useState<string[]>([]);
-  const [searchResults, setSearchResults] = useState<any[]>([]);  
-  
+  const [searchResults, setSearchResults] = useState<any[]>([]);
+  const [email, setEmail] = useState("");
+  const [showEmailPopup, setShowEmailPopup] = useState(false);
+
   useEffect(() => {
     const fetchAdvice = async () => {
       const adviceFromGlobalState = globalState.advice;
@@ -25,6 +28,30 @@ const AdvicePage = () => {
     fetchAdvice();
   }, []);
 
+  const handleEmailSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+  
+    await sendAdviceEmail(advice, searchResults, email);
+  
+    setShowEmailPopup(false);
+    setEmail("");
+  };  
+
+  const sendAdviceEmail = async (advice: string, searchResults: any[], email: string) => {
+    try {
+      const searchResultsString = searchResults.map(result => JSON.stringify(result)).join('\n');
+  
+      const response = await axios.post('/api/send-email', {
+        email: email,
+        subject: 'New Advice Generated',
+        text: `${advice}\n\nSearch Results:\n${searchResultsString}`
+      });
+      console.log(response.data.message);
+    } catch (error) {
+      console.log('Error sending advice email:', error);
+    }
+  };  
+
   return (
     <div>
       <h1>Advice</h1>
@@ -37,8 +64,21 @@ const AdvicePage = () => {
       {searchResults.map((result, index) => (
         <p key={index}>{JSON.stringify(result)}</p>
       ))}
+      <button onClick={() => setShowEmailPopup(true)}>Get Personalized Support</button>
+      {showEmailPopup && (
+        <div>
+          <form onSubmit={handleEmailSubmit}>
+            <label>
+              Email:
+              <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            </label>
+            <button type="submit">Submit</button>
+            <button type="button" onClick={() => setShowEmailPopup(false)}>Cancel</button>
+          </form>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default AdvicePage;
